ae8f89f4fe7aeeb010d8ac86923e3d98581e2785
Obieda Akilan <obieda.jehad@gmail.com>
2022-05-16 22:11:54 +0300

    context setting

---
+++ src/components/List/List.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './list.scss'
+
+import Item from '../Item/Item';
+export default function List() {
+    return (
+        <div className='list'>
+
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+
+        </div>
+    )
+}
---
+++ src/components/ToDo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Body from './Body/Body'
+import Header from './Header/Header'
+import './ToDo.scss'
+export default function ToDo() {
+    return (
+        <div className='container'>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
